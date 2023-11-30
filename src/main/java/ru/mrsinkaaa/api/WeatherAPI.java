f9790a6c94afd45145dc235e98cee4ddaeee8aef
MrSinkaaa <sinkapersinka@gmail.com>
2023-11-30 12:57:24 +0200

    First version of sending requests to Weather API

--- src/main/java/ru/mrsinkaaa/api/WeatherAPI.java
+++ src/main/java/ru/mrsinkaaa/api/WeatherAPI.java
@@ -1,17 +1,18 @@
 package ru.mrsinkaaa.api;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class WeatherAPI {
 
     private static final WeatherAPI INSTANCE = new WeatherAPI();
-
-    private WeatherAPI() {
-    }
 
     public String sendGetRequest(String apiURL) throws IOException {
 
