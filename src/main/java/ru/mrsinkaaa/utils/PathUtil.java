59e305f604d502c93a894a4c4e7ae910aff09ea4
MrSinkaaa <sinkapersinka@gmail.com>
2023-12-12 11:30:41 +0200

    Feat: Added general class with all paths

---
+++ src/main/java/ru/mrsinkaaa/utils/PathUtil.java
@@ -0,0 +1,13 @@
+package ru.mrsinkaaa.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathUtil {
+
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+    public static final String REGISTRATION = "/registration";
+    public static final String WEATHER = "/weather";
+
+}
