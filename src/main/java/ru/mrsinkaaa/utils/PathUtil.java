6680b403ea9f1b59cfa6e601d8211e03b937d5d3
MrSinkaaa <sinkapersinka@gmail.com>
2023-12-12 11:37:59 +0200

    Feat: Added images path

--- src/main/java/ru/mrsinkaaa/utils/PathUtil.java
+++ src/main/java/ru/mrsinkaaa/utils/PathUtil.java
@@ -9,5 +9,6 @@ public class PathUtil {
     public static final String LOGOUT = "/logout";
     public static final String REGISTRATION = "/registration";
     public static final String WEATHER = "/weather";
+    public static final String IMAGES = "/images";
 
 }
