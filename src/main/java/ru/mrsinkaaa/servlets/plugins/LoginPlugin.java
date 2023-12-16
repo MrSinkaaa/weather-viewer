abde43b6b2b81b8c911a7189ad9d7befcffc1e3c
MrSinkaaa <sinkapersinka@gmail.com>
2023-12-16 21:15:00 +0200

    Feat: Added handle for active session

--- src/main/java/ru/mrsinkaaa/servlets/plugins/LoginPlugin.java
+++ src/main/java/ru/mrsinkaaa/servlets/plugins/LoginPlugin.java
@@ -2,23 +2,27 @@ package ru.mrsinkaaa.servlets.plugins;
 
 import jakarta.persistence.NoResultException;
 import lombok.SneakyThrows;
-import org.thymeleaf.context.WebContext;
+import ru.mrsinkaaa.config.AppConfig;
 import ru.mrsinkaaa.config.ThymeleafConfig;
 import ru.mrsinkaaa.dto.UserDTO;
+import ru.mrsinkaaa.service.SessionService;
 import ru.mrsinkaaa.service.UserService;
 import ru.mrsinkaaa.servlets.ServletPlugin;
 import ru.mrsinkaaa.utils.PathUtil;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 import static ru.mrsinkaaa.servlets.CentralServlet.webContext;
 
 public class LoginPlugin implements ServletPlugin {
 
     private static final UserService userService = UserService.getInstance();
+    private static final SessionService sessionService = SessionService.getInstance();
 
     @Override
     public boolean canHandle(String path) {
@@ -27,18 +31,21 @@ public class LoginPlugin implements ServletPlugin {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getMethod().equals("GET")) {
 
-            ThymeleafConfig.getTemplateEngine().process("login.html", webContext, response.getWriter());
+        if(sessionService.checkIfSessionExists(request)) {
+            response.sendRedirect("/weather");
         } else {
-            try {
-                userService.login(request.getParameter("login"), request.getParameter("password"))
-                        .ifPresent(user -> onLoginSuccess(request, response, user));
-            } catch (NoResultException e) {
-                onLoginFail(request, response);
+            if (request.getMethod().equals("GET")) {
+                ThymeleafConfig.getTemplateEngine().process("login.html", webContext, response.getWriter());
+            } else {
+                try {
+                    userService.login(request.getParameter("login"), request.getParameter("password"))
+                            .ifPresent(user -> onLoginSuccess(request, response, user));
+                } catch (NoResultException e) {
+                    onLoginFail(request, response);
+                }
             }
         }
-
     }
 
     @SneakyThrows
@@ -48,7 +55,12 @@ public class LoginPlugin implements ServletPlugin {
 
     @SneakyThrows
     private void onLoginSuccess(HttpServletRequest request, HttpServletResponse response, UserDTO user) {
+        UUID session = sessionService.createSession(user);
+        Cookie cookie = new Cookie("session", session.toString());
+        cookie.setMaxAge(Integer.parseInt(AppConfig.getProperty("session.expiresAt")) * 60);
+
         request.getSession().setAttribute("user", user);
+        response.addCookie(cookie);
         response.sendRedirect("/weather");
     }
 
--- src/main/java/ru/mrsinkaaa/servlets/plugins/RegistrationPlugin.java
+++ src/main/java/ru/mrsinkaaa/servlets/plugins/RegistrationPlugin.java
@@ -1,6 +1,7 @@
 package ru.mrsinkaaa.servlets.plugins;
 
 import ru.mrsinkaaa.config.ThymeleafConfig;
+import ru.mrsinkaaa.service.SessionService;
 import ru.mrsinkaaa.service.UserService;
 import ru.mrsinkaaa.servlets.ServletPlugin;
 import ru.mrsinkaaa.utils.PathUtil;
@@ -15,6 +16,7 @@ import static ru.mrsinkaaa.servlets.CentralServlet.webContext;
 public class RegistrationPlugin implements ServletPlugin {
 
     private static final UserService userService = UserService.getInstance();
+    private static final SessionService sessionService = SessionService.getInstance();
 
     @Override
     public boolean canHandle(String path) {
@@ -23,16 +25,18 @@ public class RegistrationPlugin implements ServletPlugin {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getMethod().equals("GET")) {
-
-            ThymeleafConfig.getTemplateEngine().process("registration.html", webContext, response.getWriter());
+        if(sessionService.checkIfSessionExists(request)) {
+            response.sendRedirect("/weather");
         } else {
+            if(request.getMethod().equals("GET")) {
+                ThymeleafConfig.getTemplateEngine().process("registration.html", webContext, response.getWriter());
+            } else {
+                String login = request.getParameter("login");
+                String password = request.getParameter("password");
 
-            String login = request.getParameter("login");
-            String password = request.getParameter("password");
-
-            userService.register(login, password);
-            response.sendRedirect(PathUtil.LOGIN);
+                userService.register(login, password);
+                response.sendRedirect(PathUtil.LOGIN);
+            }
         }
 
     }
