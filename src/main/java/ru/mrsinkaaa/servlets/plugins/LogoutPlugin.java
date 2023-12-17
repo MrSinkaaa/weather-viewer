134013fb5f91141925a8d401289b8d21c961caf1
MrSinkaaa <sinkapersinka@gmail.com>
2023-12-17 12:13:58 +0200

    Feat: Clear user cookie on logout for security

--- src/main/java/ru/mrsinkaaa/servlets/plugins/LogoutPlugin.java
+++ src/main/java/ru/mrsinkaaa/servlets/plugins/LogoutPlugin.java
@@ -4,6 +4,7 @@ import ru.mrsinkaaa.servlets.ServletPlugin;
 import ru.mrsinkaaa.utils.PathUtil;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,6 +19,10 @@ public class LogoutPlugin implements ServletPlugin {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie cookie = new Cookie("session", null);
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
         request.getSession().invalidate();
         response.sendRedirect(PathUtil.LOGIN);
     }
