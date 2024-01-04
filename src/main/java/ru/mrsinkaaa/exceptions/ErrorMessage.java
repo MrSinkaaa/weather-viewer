cb2449bf6a1b803c69ce8b319bb7ec503e4b0702
MrSinkaaa <sinkapersinka@gmail.com>
2024-01-04 09:45:32 +0200

    Feat: add UserInput & UserNotFound exceptions

--- src/main/java/ru/mrsinkaaa/exceptions/ErrorMessage.java
+++ src/main/java/ru/mrsinkaaa/exceptions/ErrorMessage.java
@@ -2,6 +2,7 @@ package ru.mrsinkaaa.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import ru.mrsinkaaa.config.AppConfig;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,7 +12,11 @@ public enum ErrorMessage {
 
     USER_ALREADY_EXISTS("User with this login already exists", HttpServletResponse.SC_CONFLICT),
     USER_WRONG_CREDENTIALS("Login or password is not corrected", HttpServletResponse.SC_UNAUTHORIZED),
+    USER_WRONG_LOGIN("Login length is less than " + AppConfig.getProperty("login.length"), HttpServletResponse.SC_BAD_REQUEST),
+    USER_WRONG_PASSWORD("Password length is less than " + AppConfig.getProperty("password.length"), HttpServletResponse.SC_BAD_REQUEST),
+    USER_NOT_FOUND("User not found", HttpServletResponse.SC_NOT_FOUND),
     USER_NOT_AUTHORIZED("User is not authorized", HttpServletResponse.SC_FORBIDDEN),
+
 
     LOCATION_ALREADY_EXISTS("Location with this name already exists", HttpServletResponse.SC_CONFLICT),
     LOCATION_NOT_FOUND("Location with this name not found", HttpServletResponse.SC_NOT_FOUND);
