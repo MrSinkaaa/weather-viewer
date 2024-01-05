ea10ba06cd34439c9053dbadfd1ccd3fc18685cf
MrSinkaaa <sinkapersinka@gmail.com>
2024-01-05 16:52:55 +0200

    Feat: remove redundant method

--- src/main/java/ru/mrsinkaaa/repository/UserRepository.java
+++ src/main/java/ru/mrsinkaaa/repository/UserRepository.java
@@ -38,17 +38,6 @@ public class UserRepository implements CrudRepository<Integer, User> {
         }
     }
 
-    public Optional<User> findByLoginAndPassword(String login, String password) {
-        try (Session session = sessionFactory.openSession()) {
-            return Optional.ofNullable(session.createQuery("from User u where u.login = :login and u.password = :password", User.class)
-                    .setParameter("login", login)
-                    .setParameter("password", password)
-                    .getSingleResult());
-        } catch (NoResultException e) {
-            return Optional.empty();
-        }
-    }
-
     @Override
     public List<User> findAll() {
         try (Session session = sessionFactory.openSession()) {
